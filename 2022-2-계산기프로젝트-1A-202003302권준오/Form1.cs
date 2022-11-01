8a12bffba861f9b2333f92d712d8ecaa772bb8bd
ddjunho <112926563+ddjunho@users.noreply.github.com>
2022-11-01 16:39:35 +0900

    Add files via upload

---
+++ 2022-2-계산기프로젝트-1A-202003302권준오/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2022_2_계산기프로젝트_1A_202003302권준오
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText("1");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text+"2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText("3");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText("4");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText("5");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText("6");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText("7");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText("8");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText("9");
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+                textBox1.AppendText("0");
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+                textBox1.AppendText("000");
+        }
+        Boolean bJjeom_Flag = false;
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (bJjeom_Flag == false) 
+            {
+                if (textBox1.Text == "")
+                {
+                    textBox1.Text = "0";
+                }
+                textBox1.AppendText(".");
+                bJjeom_Flag = true;
+            }
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            bJjeom_Flag = false;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                if (textBox1.Text.Substring(textBox1.TextLength - 1) == ".")
+                {
+                    bJjeom_Flag = false;
+                }
+                if (textBox1.Text == "0.")
+                {
+                    textBox1.Text = textBox1.Text.Remove(textBox1.TextLength - 1);
+                }
+                textBox1.Text = textBox1.Text.Remove(textBox1.TextLength - 1); //Substring
+            }
+        }
+    }
+}
