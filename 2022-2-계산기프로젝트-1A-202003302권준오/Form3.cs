86e3e6dcc7aad48bc8a8108ce8908148ecb0fdb1
ddjunho <112926563+ddjunho@users.noreply.github.com>
2022-12-06 15:12:23 +0900

    Add files via upload

---
+++ 2022-2-계산기프로젝트-1A-202003302권준오/Form3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2022_2_계산기프로젝트_1A_202003302권준오
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Owner.Show();
+            this.Close();
+        }
+    }
+}
